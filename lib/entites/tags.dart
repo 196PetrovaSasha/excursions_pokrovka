a8d688378111962e1e30d8961a09366814aab467
196PetrovaSasha <90344918+196PetrovaSasha@users.noreply.github.com>
2023-07-17 12:59:09 +0300

    dfghj

---
+++ lib/entites/tags.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+var chips = [
+  const Chip(
+    backgroundColor: Colors.white,
+    side: BorderSide(
+      color: Colors.grey,
+      width: 0.5,
+    ),
+    avatar: Icon(Icons.photo_camera,
+      color: Colors.black38,),
+    label: Text('Место для фото'),
+  ),
+  const Chip(
+    backgroundColor: Colors.white,
+    side: BorderSide(
+      color: Colors.grey,
+      width: 0.5,
+    ),
+    avatar: Icon(Icons.restaurant,
+      color: Colors.black38,),
+    label: Text("Можно поесть"),
+  ),
+  const Chip(
+    backgroundColor: Colors.white,
+    side: BorderSide(
+      color: Colors.grey,
+      width: 0.5,
+    ),
+    avatar: Icon(Icons.battery_alert,
+      color: Colors.black38,),
+    label: Text('Место отдыха'),
+  ),
+  const Chip(
+    backgroundColor: Colors.white,
+    side: BorderSide(
+      color: Colors.grey,
+      width: 0.5,
+    ),
+    avatar: Icon(Icons.school,
+      color: Colors.black38,),
+    label: Text('Зона учебы'),
+  ),
+  const Chip(
+    backgroundColor: Colors.white,
+    side: BorderSide(
+      color: Colors.grey,
+      width: 0.5,
+    ),
+    avatar: Icon(
+        Icons.star_border,
+        color: Colors.black38,),
+    label: Text('Место мероприятий'),
+  ),
+  const Chip(
+    backgroundColor: Colors.white,
+    side: BorderSide(
+      color: Colors.grey,
+      width: 0.5,
+    ),
+    avatar: Icon(Icons.sports,
+      color: Colors.black38,),
+    label: Text('Место спорта'),
+  ),
+];
